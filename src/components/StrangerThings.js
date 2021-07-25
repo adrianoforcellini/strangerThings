c38c3af0276cfc661bb1d981e2f870abce3e9c96
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-25 20:11:46 -0300

    entendendo dotenv

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -4,6 +4,10 @@ import CharactersService from '../services/charactersAPI';
 import Table from './Table';
 
 dotenv.config();
+console.log(dotenv.config());
+//  config will read your .env file, parse the contents, assign it to process.env,
+//  and return an Object with a parsed key containing the loaded content or an error key if it failed.
+//  https://www.npmjs.com/package/dotenv
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
@@ -107,6 +111,8 @@ class StrangerThings extends React.Component {
   }
 
   render() {
+    const a = dotenv.config(process.env.REACT_APP_HAWKINS_URL);
+    console.log(a);
     const {
       hereIsTheUpsideDownWorld, characterName, characters, page,
     } = this.state;
